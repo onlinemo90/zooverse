8dfcb6507608711c9b00b06258062ea2537363f7
onlinemo90 <moritz.fritzsche@gmx.ch>
2020-09-02 19:19:36 +0100

    - added asset folder with DB (DB still to be imported into the app) - added empty screen for ticket scan - impl. method to call ticket scan screen

--- app/src/main/java/com/example/animalsoundsldn/MainActivity.java
+++ app/src/main/java/com/example/animalsoundsldn/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.animalsoundsldn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+//open screen for ticket scan
+    public void openScanTicket(View view) {
+        Intent intent = new Intent(getApplicationContext(), ScanTicketActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/animalsoundsldn/ScanTicketActivity.java
@@ -0,0 +1,14 @@
+package com.example.animalsoundsldn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_ticket);
+    }
+}
