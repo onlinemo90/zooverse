181eb295849ecbf4e6fd5679e820238c2ccc4912
onlinemo90 <moritz.fritzsche@gmx.ch>
2020-10-25 00:42:00 +0100

    Species Details screen Search results list clickable Audio Files playable (Giraffe currently has an example)
    Ticket List
    casting removed where not needed

---
+++ app/src/main/java/com/zooverse/activities/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package com.zooverse.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AbstractBaseActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		startActivity(new Intent(this, MainActivity.class));
+		finish();
+	}
+}
