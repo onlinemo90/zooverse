8da4874ccba03a45d2edaadcc5d4e2d0f9a48650
Alyph <pmb.ferreira93@gmail.com>
2020-10-25 14:01:52 +0000

    Theme changes:  - SplashTheme now sets the status bar color to transparent  - SplashTheme background colour now set to @color/colorBackground_dark. Reason for this is users with dark theme would get blinded by a light background, whereas Light Theme users would not really be affected (plus the lion would be more noticeable)  - Explicitly set the Status Bar colour in the Base Theme, in case we want to change it in the future. Currently set to the default of themeColorPrimaryDark, so this doesn't change anything

--- app/src/main/java/com/zooverse/activities/SplashScreenActivity.java
+++ app/src/main/java/com/zooverse/activities/SplashScreenActivity.java
@@ -3,7 +3,7 @@ package com.zooverse.activities;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashScreenActivity extends AbstractBaseActivity{
+public class SplashScreenActivity extends AbstractBaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
