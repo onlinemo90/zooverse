3c627fd6d7b4e9706c5f63d72bf83f1f9d002b94
onlinemo90 <moritz.fritzsche@gmx.ch>
2020-11-15 18:19:50 +0000

    App can be called via url

--- app/src/main/java/com/zooverse/activities/SplashScreenActivity.java
+++ app/src/main/java/com/zooverse/activities/SplashScreenActivity.java
@@ -2,11 +2,25 @@ package com.zooverse.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import com.zooverse.R;
+import com.zooverse.model.Model;
+import com.zooverse.model.Ticket;
 
 public class SplashScreenActivity extends AbstractBaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		try {
+			String[] ticketContent = getIntent().getData().toString().split("ticket=");
+			Ticket ticket = new Ticket(ticketContent[1]);
+			if (ticket.isValid() && ticket.getZooID().equals(getString(R.string.zoo_id)) && !Model.getStoredTickets().contains(ticket)) {
+				Model.storeTicket(ticket);
+			}
+			Log.d("entryPoint", "Browser");
+		} catch (Exception e) {
+			Log.d("entryPoint", "Launcher");
+		}
 		startActivity(new Intent(this, MainActivity.class));
 		finish();
 	}
