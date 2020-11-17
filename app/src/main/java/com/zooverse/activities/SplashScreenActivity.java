fabbe43c2deca260fd808149e3826c69aaa5c4c1
Pedro Ferreira <pmb.ferreira93@gmail.com>
2020-11-17 22:40:33 +0000

    Added support for URL-based entry point by parsing out the GET request from the URL, which should contain the encrypted API request

--- app/src/main/java/com/zooverse/activities/SplashScreenActivity.java
+++ app/src/main/java/com/zooverse/activities/SplashScreenActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.regex.Pattern;
+
 public class SplashScreenActivity extends AbstractBaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		try {
-			super.processExternalRequest(getIntent().getData().toString());
+			super.processExternalRequest(getIntent().getData().toString().split(Pattern.quote("?"))[1]);
 			Log.d("entryPoint", "Browser");
 		} catch (NullPointerException npe) {
 			Log.d("entryPoint", "Launcher");
